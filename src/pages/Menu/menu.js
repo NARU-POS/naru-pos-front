28dc931df50641ee3859a4a798d3e007dbe0943a
handwoong <handwoong@gmail.com>
2022-11-09 19:24:07 +0900

    feat: 메뉴판 스켈레톤 코드 작성

---
+++ src/pages/Menu/menu.js
@@ -0,0 +1,96 @@
+import { useState, useEffect } from "react";
+import AppBar from "@mui/material/AppBar";
+import Button from "@mui/material/Button";
+import RestaurantMenuIcon from "@mui/icons-material/RestaurantMenu";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { get } from "../../utils/api";
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const theme = createTheme();
+
+export default function Menu() {
+  const [menus, setMenus] = useState([]);
+  const [category, setCategory] = useState([]);
+
+  useEffect(() => {
+    async function getMenuList() {
+      const res = await get("menus");
+      console.log(res);
+      if (res.status !== 200) console.log("잘못된 요청");
+      setMenus(res.data);
+      const filterCategory = res.data.map((menu) => menu.category);
+      const setMenuCategory = new Set(filterCategory);
+      setCategory([...setMenuCategory]);
+    }
+
+    getMenuList();
+  }, []);
+
+  console.log("메뉴", menus);
+  console.log("카테고리", category);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="fixed">
+        <Toolbar>
+          <RestaurantMenuIcon sx={{ mr: 2 }} />
+          <Typography color="inherit" noWrap>
+            Naru Restaurant & Coffee
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            bgcolor: "background.paper",
+            pt: 1,
+            pb: 1,
+          }}
+        ></Box>
+        <Container sx={{ py: 8 }} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      // 16:9
+                      pt: "56.25%",
+                    }}
+                    image="https://source.unsplash.com/random"
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading
+                    </Typography>
+                    <Typography>This is a media card. You can use this section to describe the content.</Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">View</Button>
+                    <Button size="small">Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+    </ThemeProvider>
+  );
+}
