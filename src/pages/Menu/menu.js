2d6224269c0c653a941a6e23e53f49f856dc5f29
handwoong <handwoong@gmail.com>
2022-11-10 17:37:45 +0900

    feat: 메뉴판 카테고리 기능 추가

--- src/pages/Menu/menu.js
+++ src/pages/Menu/menu.js
@@ -1,49 +1,50 @@
 import { useState, useEffect } from "react";
 import AppBar from "@mui/material/AppBar";
-import Button from "@mui/material/Button";
 import RestaurantMenuIcon from "@mui/icons-material/RestaurantMenu";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import CssBaseline from "@mui/material/CssBaseline";
 import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { get } from "../../utils/api";
-
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const theme = createTheme();
 
 export default function Menu() {
+  const [tabsIndex, setTabsIndex] = useState(0);
   const [menus, setMenus] = useState([]);
-  const [category, setCategory] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [currentCategory, setCurrentCategory] = useState("stake");
+
+  const handleChange = (_event, categoryIndex) => {
+    setTabsIndex(categoryIndex);
+    setCurrentCategory(categories[categoryIndex]);
+  };
 
   useEffect(() => {
     async function getMenuList() {
       const res = await get("menus");
-      console.log(res);
       if (res.status !== 200) console.log("잘못된 요청");
-      setMenus(res.data);
+
       const filterCategory = res.data.map((menu) => menu.category);
       const setMenuCategory = new Set(filterCategory);
-      setCategory([...setMenuCategory]);
+      setMenus(res.data);
+      setCategories([...setMenuCategory]);
     }
 
     getMenuList();
   }, []);
 
-  console.log("메뉴", menus);
-  console.log("카테고리", category);
-
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <AppBar position="fixed">
+      <AppBar position="static">
         <Toolbar>
           <RestaurantMenuIcon sx={{ mr: 2 }} />
           <Typography color="inherit" noWrap>
@@ -51,43 +52,33 @@ export default function Menu() {
           </Typography>
         </Toolbar>
       </AppBar>
+      <AppBar position="sticky" sx={{ bgcolor: "white" }}>
+        <Tabs value={tabsIndex} onChange={handleChange} variant="scrollable" scrollButtons={false} aria-label="naru-menu-category">
+          {categories.map((category) => (
+            <Tab key={category} label={category} />
+          ))}
+        </Tabs>
+      </AppBar>
       <main>
-        {/* Hero unit */}
-        <Box
-          sx={{
-            bgcolor: "background.paper",
-            pt: 1,
-            pb: 1,
-          }}
-        ></Box>
         <Container sx={{ py: 8 }} maxWidth="md">
-          {/* End hero unit */}
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
-                  <CardMedia
-                    component="img"
-                    sx={{
-                      // 16:9
-                      pt: "56.25%",
-                    }}
-                    image="https://source.unsplash.com/random"
-                    alt="random"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                    <Typography>This is a media card. You can use this section to describe the content.</Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small">View</Button>
-                    <Button size="small">Edit</Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
+          <Grid container spacing={3}>
+            {menus.map(
+              (menu) =>
+                menu.category === currentCategory && (
+                  <Grid item key={menu._id} xs={12} sm={6} md={4}>
+                    <Card sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
+                      <CardMedia component="img" image={menu.photo_url} alt="random" />
+                      <CardContent sx={{ flexGrow: 1 }}>
+                        <Typography gutterBottom variant="h5" component="h2">
+                          {menu.name}
+                        </Typography>
+                        <Typography sx={{ mb: 1 }}>{menu.description}</Typography>
+                        <Typography align="right">₩ {menu.price.toLocaleString("ko-KR")}</Typography>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                )
+            )}
           </Grid>
         </Container>
       </main>
